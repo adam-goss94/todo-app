d0de0039a9578628c2b321d9a8b36475e8d32b77
Adam Goss <adam.goss94@gmail.com>
2020-06-24 17:45:22 +0200

    Add socket to server

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const path = require('path');
+const socket = require('socket.io');
 
 const app = express();
+let tasks = [];
+
+app.use(express.static(path.join(__dirname, '/client')));
+
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running...');
 });
@@ -8,3 +14,17 @@ const server = app.listen(process.env.PORT || 8000, () => {
 app.use((req, res) => {
   res.status(404).send({ message: 'Not found...' });
 });
+
+const io = socket(server);
+
+io.on('connection', () => {
+  socket.emit('updateData', tasks);
+  socket.on('addTask', (newTask) => {
+    tasks.push(newTask);
+    socket.broadcast.emit('addTask', newTask);
+  });
+  socket.on('removeTask', (taskId) => {
+    tasks.splice(taskId);
+    socket.broadcast.emit('removeTask', taskId);
+  });
+}); 
